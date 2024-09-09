82da0f9bb234d5c0e1cc4cbfd54e7b66e3842df4
Dmitry Vodennikov <dvodennikov@gmail.com>
2024-09-09 17:32:21 +0500

    Update webpack config
    * Update dev and prod scripts
    * Remove development and production scripts
    * Rename src/document-type-custom-fields.js to src/custom-fields.js
    * Fix backend/assets/AppAsset.php to use backend/js/custom-fields.js

--- webpack/webpack.development.js
+++ webpack/webpack.development.js
@@ -1,8 +1,24 @@
 const { merge } = require('webpack-merge');
-const common = require('./webpack.config.js');
+const path = require('path');
+const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
 	mode: 'development',
+	module: {
+		rules: [
+			{
+				test: /\.(c|ca|cs)ss$/i,
+				use: [
+					'style-loader',
+					'css-loader',
+					//'postcss-loader',
+					//'sass-loader,'
+				],
+				include: path.resolve(__dirname, '../src/'),
+				sideEffects: true,
+			},
+		],
+	},
 	watch: true,
 	watchOptions: {
 		ignored: ['./vendor/**', './frontend/**', './node_modules/**'],
--- webpack/webpack.production.js
+++ webpack/webpack.production.js
@@ -1,7 +1,27 @@
 const { merge } = require('webpack-merge');
-const common = require('./webpack.config.js');
+const path = require('path');
+const miniCssExtractPlugin = require('mini-css-extract-plugin');
+const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
 	mode: 'production',
-	
+	entry: {
+		site: {import: '../src/site.css', filename: 'css/[name].css'},
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(c|ca|cs)ss$/i,
+				use: [
+					miniCssExtractPlugin.loader,
+					'css-loader',
+					//'postcss-loader',
+					//'sass-loader,'
+				],
+				include: path.resolve(__dirname, '../src/'),
+				sideEffects: true,
+			},
+		],
+	},
+	plugins: [new miniCssExtractPlugin()],
 });
